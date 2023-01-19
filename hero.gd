41b8f0ead3973a02905fad572aab942edb883abe
StrayRaider <41emrekaya919@gmail.com>
2023-01-19 15:47:35 +0300

    karakterin klavye tuşlarına göre haraket etme kodu yazıldı

--- hero.gd
+++ hero.gd
@@ -5,6 +5,7 @@ var height = OS.get_window_size().y
 var velocity = 100
 var velo_treshold = 100
 var accelartion = 9.81*5
+var active_direction = null #up down right left
 
 func _enter_tree():
 	print("I entered")
@@ -22,10 +23,30 @@ func _physics_process(delta):
 	#set_it_left_centered()
 	#set_it_right_centered()
 	#accelartion_down(delta)
-	if()
-	move_left(delta)
-	move_right(delta)
+	#move_left(delta)
+	#move_right(delta)
+	if active_direction == "up":
+		move_up(delta)
+	elif active_direction == "down":
+		move_down(delta)
+	elif active_direction == "left":
+		move_left(delta)
+	elif active_direction == "right":
+		move_right(delta)
 
+func _input(event):
+	if event is InputEventKey:
+		if event.get_scancode() == KEY_W:
+			active_direction = "up"
+		elif event.get_scancode() == KEY_S:
+			active_direction = "down"
+		elif event.get_scancode() == KEY_A:
+			active_direction = "left"
+		elif event.get_scancode() == KEY_D:
+			active_direction = "right"
+		else :
+			active_direction = null
+	print(event)
 
 func move_right(delta) -> void:
 	self.position.x += velocity*delta
